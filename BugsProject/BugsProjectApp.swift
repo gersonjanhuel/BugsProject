4b841daaed22a8b6e73010dbddcb2bd2e0f3f2b9
Gerson Janhuel <gersonjanhuel@gmail.com>
2024-09-24 10:43:01 +0700

    Initial Commit

---
+++ BugsProject/BugsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugsProjectApp.swift
+//  BugsProject
+//
+//  Created by Gerson Janhuel on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BugsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BugsProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BugsProject
+//
+//  Created by Gerson Janhuel on 24/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
